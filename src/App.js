138d8476759455449c3b4d855e66baf1af5f21be
TuyenPhamm <phamquangtuyen0704@gmail.com>
2023-03-27 14:33:27 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+axios.get('https://jsonplaceholder.typicode.com/posts')
+.then((res)=>{
+  console.log(res)
+})
 
+fetch
+var postApi = 'https://jsonplaceholder.typicode.com/posts'
+fetch(postApi)
+
+.then(function(reponse){
+    return reponse.json();
+})
+.then(function(post){
+    console.log(post)
+})
+.catch(function(err){
+    console.log('Lỗi')
+})
 function App() {
   return (
     <div className="App">
